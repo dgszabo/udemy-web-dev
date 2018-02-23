5878959f4391a52120af76184d917d0ca2d4889d
Daniel Szabo <dgszabo@gmail.com>
2018-02-23 10:39:00 -0800

    Unit 16 - jQuery - Todo List - delete functionality added

--- Unit 16 - jQuery/Todo-List-Project/Resources/JS/todo-list.js
+++ Unit 16 - jQuery/Todo-List-Project/Resources/JS/todo-list.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
+	// toggle class (grey + strike-through) when clicked
 	$('li').on('click', function() {
 		$(this).toggleClass('completed');
+	});
+
+	// create delete functionality
+	$('span').on('click', function(event) {
+		$(event.target).parent().remove();
+		event.stopPropagation();
 	});
 });

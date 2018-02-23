dacc8b281654cdf6f0ed9c5257ad694ea59185b7
Daniel Szabo <dgszabo@gmail.com>
2018-02-23 13:46:41 -0800

    Unit 16 - jQuery - Todo List - new item functionality added

--- Unit 16 - jQuery/Todo-List-Project/Resources/JS/todo-list.js
+++ Unit 16 - jQuery/Todo-List-Project/Resources/JS/todo-list.js
@@ -1,14 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(){
 	// toggle class (grey + strike-through) when clicked
-	$('li').on('click', function() {
+	$('ul').on('click', 'li', function() {
 		$(this).toggleClass('completed');
 	});
 
 	// create delete functionality
-	$('span').on('click', function(event) {
+	$('ul').on('click', 'span', function(event) {
 		$(event.target).parent().fadeOut(500, function() {
 			$(this).fadeOut();
 		});
 		event.stopPropagation();
+	});
+
+	// create ability to add todos
+	$('input[type="text"]').keypress(function(event) {
+		if(event.which === 13) {
+			var newTodo = $(this).val();
+			$(this).val('');
+			$('ul').append('<li><span>X</span> ' + newTodo + '</li>');
+		}
 	});
 });
